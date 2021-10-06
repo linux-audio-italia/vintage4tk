6d93411feb431d09250906d3defc9e012911a592
Ascani Carlo <carlotm@pm.me>
2021-10-06 11:43:57 +0200

    Add breadcrumbs

--- apps/recorders/views.py
+++ apps/recorders/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView
 
 from .models import Brand, Recorder
@@ -12,6 +13,12 @@ class BrandDetailView(DetailView):
     context_object_name = "brand"
     model = Brand
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_brand = self.get_object()
+        context["breadcrumbs"] = [("home", reverse_lazy("brand-list")), (current_brand.name, None)]
+        return context
+
 
 class RecorderDetailView(DetailView):
     context_object_name = "recorder"
@@ -21,3 +28,13 @@ class RecorderDetailView(DetailView):
         return Recorder.objects.filter(
             slug=self.kwargs.get(self.slug_field), brand__slug=self.kwargs.get("brand_slug")
         )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        recorder = self.get_object()
+        context["breadcrumbs"] = [
+            ("home", reverse_lazy("brand-list")),
+            (recorder.brand.name, reverse_lazy("brand-detail", args=[recorder.brand.slug])),
+            (recorder.model, None),
+        ]
+        return context
