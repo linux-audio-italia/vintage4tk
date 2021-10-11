e767f0c87e3748df26e66a66702729a0a7452a2e
Ascani Carlo <carlotm@pm.me>
2021-10-11 08:47:19 +0200

    Add a sidebar with all brands

--- apps/recorders/templatetags/breadcrumbs.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.inclusion_tag("includes/breadcrumbs.html", takes_context=True)
-def breadcrumbs(context):
-    return {"breadcrumbs": context["breadcrumbs"]}
